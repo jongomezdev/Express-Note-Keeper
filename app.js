e53b5815b9e79fb2aa461717af52dd24a2f9e9de
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-26 16:18:07 -0500

    set up express server

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const fs = require("fs");
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
