a623a6f7f9c20a67ae39b1d53f4237c9887381a2
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-28 17:02:01 -0500

    changed app.js to server.js

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "notes.html"))
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
 );
-
-app.get("/notes", (req, res) => {
-  res.send("public/notes");
-});
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
 
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
