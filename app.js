003a9f15ff98ffca4473f8fb621f032bdbaacc16
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-28 16:36:03 -0500

    added pseudo code and get methods

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "notes.html"))
+);
+
+app.get("/notes", (req, res) => {
+  res.send("public/notes");
 });
+
+app.listen(PORT, () => {
+  console.log("App listening on PORT " + PORT);
+});
+// GET /notes - list all notes
+// POST /notes - Create a new note
+// GET /notes/:id - Get one note (using ID)
+// PATCH /note/:id - Update one note
+// DELETE /note/:id - Destroy one note
