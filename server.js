2d92598077acc8d1e16c751bf7a2be52a8ba1920
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-30 17:23:09 -0500

    get request for the api/notes

--- server.js
+++ server.js
@@ -11,30 +11,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+//****************
 // HTML GET ROUTES
+//****************
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
-
-// app.get("/api/notes", (req, res) => {
-//   const notes = getNotes().map((note, uuid) => {
-//     return {
-//       id: uuid(),
-//       title: note.title,
-//       text: note.text,
-//     };
-//   });
-//   res.write(notes);
-// });
-
-// app.get("/api/notes", (req, res) => {
-//   fs.readFile("db/db.json", "utf-8");
-//   let notes = JSON.parse(data);
-//   res.json(notes);
-// });
+//********************
+//Get /api/notes route
+//********************
+app.get("/api/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf-8");
+  let notes = JSON.parse(data);
+  res.json(notes);
+});
 
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
