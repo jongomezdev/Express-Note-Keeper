2505f889f74e6ab8b83b73e40a40ac9fa2d2e581
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-30 17:14:43 -0500

    Resolved issue with setting index.html to *

--- server.js
+++ server.js
@@ -2,32 +2,39 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const { v4: uuid } = require("uuid");
-const { text } = require("express");
+const db = "db/db.json";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-// GET
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/index.html"))
-);
+// HTML GET ROUTES
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
 
-app.get("/api/notes", (req, res) => {
-  const notes = getNotes().map((note, uuid) => {
-    return {
-      id: uuid(),
-      title: note.title,
-      text: note.text,
-    };
-  });
-  res.send(notes);
-});
+// app.get("/api/notes", (req, res) => {
+//   const notes = getNotes().map((note, uuid) => {
+//     return {
+//       id: uuid(),
+//       title: note.title,
+//       text: note.text,
+//     };
+//   });
+//   res.write(notes);
+// });
+
+// app.get("/api/notes", (req, res) => {
+//   fs.readFile("db/db.json", "utf-8");
+//   let notes = JSON.parse(data);
+//   res.json(notes);
+// });
 
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
