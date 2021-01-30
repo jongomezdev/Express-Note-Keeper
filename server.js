9ba220465c177de11e9fbb9313a2398de48ef652
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-29 20:17:18 -0500

    Create get route to render saved notes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const { v4: uuid } = require("uuid");
+const { text } = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,12 +10,24 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("*", (req, res) =>
+// GET
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
+
+app.get("/api/notes", (req, res) => {
+  const notes = getNotes().map((note, uuid) => {
+    return {
+      id: uuid(),
+      title: note.title,
+      text: note.text,
+    };
+  });
+  res.send(notes);
+});
 
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
